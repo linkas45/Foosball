ec21b083e6a8bb77f911ccd70b8834233ce37c79
Migle <Migle@DESKTOP-M95QG4F>
2017-09-21 21:24:55 +0300

    Add video file and ball detection

--- Foosball/Form1.Designer.cs
+++ Foosball/Form1.Designer.cs
@@ -29,11 +29,110 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            this.tableLayoutPanelOuter = new System.Windows.Forms.TableLayoutPanel();
+            this.ibOriginal = new Emgu.CV.UI.ImageBox();
+            this.ibThresh = new Emgu.CV.UI.ImageBox();
+            this.textBoxXYRadius = new System.Windows.Forms.TextBox();
+            this.tableLayoutPanelInner = new System.Windows.Forms.TableLayoutPanel();
+            this.tableLayoutPanelOuter.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.ibOriginal)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ibThresh)).BeginInit();
+            this.tableLayoutPanelInner.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanelOuter
+            // 
+            this.tableLayoutPanelOuter.ColumnCount = 2;
+            this.tableLayoutPanelOuter.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanelOuter.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanelOuter.Controls.Add(this.ibOriginal, 0, 0);
+            this.tableLayoutPanelOuter.Controls.Add(this.ibThresh, 1, 0);
+            this.tableLayoutPanelOuter.Controls.Add(this.tableLayoutPanelInner, 0, 1);
+            this.tableLayoutPanelOuter.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanelOuter.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanelOuter.Name = "tableLayoutPanelOuter";
+            this.tableLayoutPanelOuter.RowCount = 2;
+            this.tableLayoutPanelOuter.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 80F));
+            this.tableLayoutPanelOuter.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.tableLayoutPanelOuter.Size = new System.Drawing.Size(1436, 577);
+            this.tableLayoutPanelOuter.TabIndex = 0;
+            // 
+            // ibOriginal
+            // 
+            this.ibOriginal.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.ibOriginal.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.ibOriginal.Enabled = false;
+            this.ibOriginal.Location = new System.Drawing.Point(3, 3);
+            this.ibOriginal.Name = "ibOriginal";
+            this.ibOriginal.Size = new System.Drawing.Size(712, 455);
+            this.ibOriginal.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.ibOriginal.TabIndex = 2;
+            this.ibOriginal.TabStop = false;
+            // 
+            // ibThresh
+            // 
+            this.ibThresh.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.ibThresh.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.ibThresh.Enabled = false;
+            this.ibThresh.Location = new System.Drawing.Point(721, 3);
+            this.ibThresh.Name = "ibThresh";
+            this.ibThresh.Size = new System.Drawing.Size(712, 455);
+            this.ibThresh.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.ibThresh.TabIndex = 2;
+            this.ibThresh.TabStop = false;
+            // 
+            // textBoxXYRadius
+            // 
+            this.textBoxXYRadius.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.textBoxXYRadius.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxXYRadius.Location = new System.Drawing.Point(3, 3);
+            this.textBoxXYRadius.Multiline = true;
+            this.textBoxXYRadius.Name = "textBoxXYRadius";
+            this.textBoxXYRadius.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.textBoxXYRadius.Size = new System.Drawing.Size(1424, 104);
+            this.textBoxXYRadius.TabIndex = 2;
+            this.textBoxXYRadius.WordWrap = false;
+            // 
+            // tableLayoutPanelInner
+            // 
+            this.tableLayoutPanelInner.ColumnCount = 2;
+            this.tableLayoutPanelOuter.SetColumnSpan(this.tableLayoutPanelInner, 2);
+            this.tableLayoutPanelInner.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+            this.tableLayoutPanelInner.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanelInner.Controls.Add(this.textBoxXYRadius, 1, 0);
+            this.tableLayoutPanelInner.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanelInner.Location = new System.Drawing.Point(3, 464);
+            this.tableLayoutPanelInner.Name = "tableLayoutPanelInner";
+            this.tableLayoutPanelInner.RowCount = 1;
+            this.tableLayoutPanelInner.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanelInner.Size = new System.Drawing.Size(1430, 110);
+            this.tableLayoutPanelInner.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1436, 577);
+            this.Controls.Add(this.tableLayoutPanelOuter);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.tableLayoutPanelOuter.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.ibOriginal)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ibThresh)).EndInit();
+            this.tableLayoutPanelInner.ResumeLayout(false);
+            this.tableLayoutPanelInner.PerformLayout();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanelOuter;
+        private Emgu.CV.UI.ImageBox ibOriginal;
+        private Emgu.CV.UI.ImageBox ibThresh;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanelInner;
+        private System.Windows.Forms.TextBox textBoxXYRadius;
     }
 }
 
--- Foosball/Form1.cs
+++ Foosball/Form1.cs
@@ -8,13 +8,90 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+
+using Emgu.CV;                  //
+using Emgu.CV.CvEnum;           // usual Emgu CV imports
+using Emgu.CV.Structure;        //
+using Emgu.CV.UI;               //
+using Emgu.CV.Cvb;
+
 namespace Foosball
 {
     public partial class Form1 : Form
     {
+
+        VideoCapture capture;
+
+
         public Form1()
         {
             InitializeComponent();
         }
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Filter = "Video Files |*.mp4";
+            if(ofd.ShowDialog()==DialogResult.OK)
+            {
+                capture = new Emgu.CV.VideoCapture(ofd.FileName);
+            }
+            Application.Idle += ProcessFrameAndUpdateGUI;
+        }
+
+        private void ProcessFrameAndUpdateGUI(object sender, EventArgs e)
+        {
+
+            Mat imgOriginal = capture.QueryFrame();
+
+            if (imgOriginal == null)
+            {
+                Environment.Exit(0);
+                return;
+            }
+
+            Mat imgHSV = new Mat(imgOriginal.Size, DepthType.Cv8U, 3);
+
+            Mat imgThreshLow = new Mat(imgOriginal.Size, DepthType.Cv8U, 1);
+            Mat imgThreshHigh = new Mat(imgOriginal.Size, DepthType.Cv8U, 1);
+
+            Mat imgThresh = new Mat(imgOriginal.Size, DepthType.Cv8U, 1);
+
+            CvInvoke.CvtColor(imgOriginal, imgHSV, ColorConversion.Bgr2Hsv); //iš 
+
+            CvInvoke.InRange(imgHSV, new ScalarArray(new MCvScalar(0, 155, 155)), new ScalarArray(new MCvScalar(20, 255, 255)), imgThreshLow);
+            CvInvoke.InRange(imgHSV, new ScalarArray(new MCvScalar(0, 155, 155)), new ScalarArray(new MCvScalar(20, 255, 255)), imgThreshHigh);
+
+            CvInvoke.Add(imgThreshLow, imgThreshHigh, imgThresh);
+
+            CvInvoke.GaussianBlur(imgThresh, imgThresh, new Size(3, 3), 0);
+
+            Mat structuringElement = CvInvoke.GetStructuringElement(ElementShape.Rectangle, new Size(3, 3), new Point(-1, -1));
+
+            CvInvoke.Dilate(imgThresh, imgThresh, structuringElement, new Point(-1, -1), 1, BorderType.Default, new MCvScalar(0, 0, 0));
+            CvInvoke.Erode(imgThresh, imgThresh, structuringElement, new Point(-1, -1), 1, BorderType.Default, new MCvScalar(0, 0, 0));
+
+            CircleF[] circles = CvInvoke.HoughCircles(imgThresh, HoughType.Gradient, 2.0, imgThresh.Rows / 4, 100, 50, 10, 400);
+
+            foreach (CircleF circle in circles)
+            {
+                if (textBoxXYRadius.Text != "")
+                {                         // if we are not on the first line in the text box
+                    textBoxXYRadius.AppendText(Environment.NewLine);         // then insert a new line char
+                }
+
+                textBoxXYRadius.AppendText("ball position x = " + circle.Center.X.ToString().PadLeft(4) + ", y = " + circle.Center.Y.ToString().PadLeft(4) + ", radius = " + circle.Radius.ToString("###.000").PadLeft(7));
+                textBoxXYRadius.ScrollToCaret();             // scroll down in text box so most recent line added (at the bottom) will be shown
+
+                CvInvoke.Circle(imgOriginal, new Point((int)circle.Center.X, (int)circle.Center.Y), (int)circle.Radius, new MCvScalar(0, 0, 255), 2);
+                CvInvoke.Circle(imgOriginal, new Point((int)circle.Center.X, (int)circle.Center.Y), 3, new MCvScalar(0, 255, 0), -1);
+            }
+
+            ibOriginal.Image = imgOriginal;
+            ibThresh.Image = imgThresh;
+
+        }
+
     }
 }
