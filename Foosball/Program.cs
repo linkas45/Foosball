47a4aed5de7116bbf795883d87d6875a8d928e59
Matas Kaminskas <mataskam@gmail.com>
2017-09-22 08:45:04 +0300

    Tested a bit

--- Foosball/Program.cs
+++ Foosball/Program.cs
@@ -17,7 +17,7 @@ namespace Foosball
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
-//            test
+//            testksdfhsdjkh
         }
     }
 }
