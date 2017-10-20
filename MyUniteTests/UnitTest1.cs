fbbe6ad69248f0a1add851660fdce05ce61b13d2
Matas Kaminskas <mataskam@gmail.com>
2017-10-20 10:35:52 +0300

    Fixed project

---
+++ MyUniteTests/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MyUniteTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
