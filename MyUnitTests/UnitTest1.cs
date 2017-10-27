0f68cc9aa4dce0af98063ccae847c3e59172eb32
Matas Kaminskas <mataskam@gmail.com>
2017-10-27 03:20:34 +0300

    Unit Tests Added

--- MyUnitTests/UnitTest1.cs
+++ MyUnitTests/UnitTest1.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Foosball;
+using Foosball.Utils;
 
 namespace MyUnitTests
 {
@@ -8,13 +9,82 @@ namespace MyUnitTests
     public class UnitTest1
     {
         [TestMethod]
-        public void wasGoalTest()
+        public void CorrectRanking()
         {
-            String filePath = "./Resources/VID_20171020_100012.mp4";
+            Team team1 = new Team("good_team", 1600);
+            Team team2 = new Team("bad_team", 400);
+            int GoalCount1 = 3, GoalCount2 = 0;
+            ScoreInput scoreInput = new ScoreInput(GoalCount1, GoalCount2, "good_team", "bad_team");
+            scoreInput.CalculateRanking(ref team1, ref team2, GoalCount1, GoalCount2);
+            Assert.IsTrue((team1.GlobalScore > 1600 && team2.GlobalScore < 400) ? true : false, "Failed");
+        }
 
-            //Counting_Goals goal = new Counting_Goals();
-            bool success = true; //goal.wasGoal(filePath);
-            Assert.AreEqual(1, success);
+        [TestMethod]
+        public void CorrectRanking2()
+        {
+            Team team1 = new Team("good_team", 1100);
+            Team team2 = new Team("bad_team", 1000);
+            int GoalCount1 = 2, GoalCount2 = 0;
+            ScoreInput scoreInput = new ScoreInput(GoalCount1, GoalCount2, "good_team", "bad_team");
+            scoreInput.CalculateRanking(ref team1, ref team2, GoalCount1, GoalCount2);
+            Assert.IsTrue((team1.GlobalScore > 1100 && team2.GlobalScore < 1000) ? true : false, "Failed");
+        }
+
+        [TestMethod]
+        public void CorrectRanking3()
+        {
+            Team team1 = new Team("good_team", 1600);
+            Team team2 = new Team("bad_team", 400);
+            int GoalCount1 = 0, GoalCount2 = 5;
+            ScoreInput scoreInput = new ScoreInput(GoalCount1, GoalCount2, "good_team", "bad_team");
+            scoreInput.CalculateRanking(ref team1, ref team2, GoalCount1, GoalCount2);
+            Assert.IsTrue((team1.GlobalScore < 1500 && team2.GlobalScore > 500) ? true : false, "Failed");
+        }
+
+        [TestMethod]
+        public void CorrectRanking4()
+        {
+            Team team1 = new Team("good_team", 700);
+            Team team2 = new Team("bad_team", 400);
+            int GoalCount1 = 3, GoalCount2 = 0;
+            ScoreInput scoreInput = new ScoreInput(GoalCount1, GoalCount2, "good_team", "bad_team");
+            scoreInput.CalculateRanking(ref team1, ref team2, GoalCount1, GoalCount2);
+            Assert.IsTrue((team1.GlobalScore > 700 && team2.GlobalScore < 400) ? true : false, "Failed");
+        }
+
+        [TestMethod]
+        public void CorrectRanking5()
+        {
+            Team team1 = new Team("good_team", 1600);
+            Team team2 = new Team("bad_team", 400);
+            int GoalCount1 = 3, GoalCount2 = 10;
+            ScoreInput scoreInput = new ScoreInput(GoalCount1, GoalCount2, "good_team", "bad_team");
+            scoreInput.CalculateRanking(ref team1, ref team2, GoalCount1, GoalCount2);
+            Assert.IsTrue((team1.GlobalScore < 1400 && team2.GlobalScore > 600) ? true : false, "Failed");
+        }
+
+
+        [TestMethod]
+        public void CorrectRanking6()
+        {
+            Team team1 = new Team("good_team", 100);
+            Team team2 = new Team("bad_team", 200);
+            int GoalCount1 = 3, GoalCount2 = 0;
+            ScoreInput scoreInput = new ScoreInput(GoalCount1, GoalCount2, "good_team", "bad_team");
+            scoreInput.CalculateRanking(ref team1, ref team2, GoalCount1, GoalCount2);
+            Assert.IsTrue((team1.GlobalScore > 400 && team2.GlobalScore < 50) ? true : false, "Failed");
+        }
+
+
+        [TestMethod]
+        public void CorrectRanking7()
+        {
+            Team team1 = new Team("good_team", 100);
+            Team team2 = new Team("bad_team", 1000);
+            int GoalCount1 = 13, GoalCount2 = 0;
+            ScoreInput scoreInput = new ScoreInput(GoalCount1, GoalCount2, "good_team", "bad_team");
+            scoreInput.CalculateRanking(ref team1, ref team2, GoalCount1, GoalCount2);
+            Assert.IsTrue((team1.GlobalScore > 2000 && team2.GlobalScore < 100) ? true : false, "Failed");
         }
     }
 }
