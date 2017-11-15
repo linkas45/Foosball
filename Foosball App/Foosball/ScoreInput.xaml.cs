bc3f4962150184cec76a2914af9f5e2b9a2f9e01
Simonas <simonas.butkus@mif.stud.vu.lt>
2017-11-15 02:31:25 +0200

    Add a few Xamarin forms

---
+++ Foosball App/Foosball/ScoreInput.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Foosball
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class ScoreInput : ContentPage
+	{
+        public ScoreInput()
+        {
+            InitializeComponent();
+        }
+
+		public ScoreInput (int team1Score, int team2Score)
+		{
+			InitializeComponent ();
+
+            team1ScoreEntry.Text = team1Score.ToString();
+            team2ScoreEntry.Text = team2Score.ToString();
+
+		}
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ Foosball App/Foosball/VideoScreen.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Xamarin.Forms;
+
+namespace Foosball
+{
+    public class VideoScreen : ContentView
+    {
+        public VideoScreen()
+        {
+            Content = new StackLayout
+            {
+                Children = {
+                    new Label { Text = "Welcome to Xamarin Forms!" }
+                }
+            };
+        }
+    }
+}
