5215f5c16b25a7a3df84b681498e99b0e4c42aed
Migle <Migle@DESKTOP-M95QG4F>
2017-11-15 16:07:23 +0200

    Add VideoFile class

---
+++ Foosball App/Foosball/VideoFile.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Emgu.CV;
+using Emgu.CV.CvEnum;
+using Emgu.CV.Structure;
+using System.IO;
+
+namespace Foosball
+{
+    public class VideoFile
+    {
+        public VideoCapture capture;
+
+        public VideoFile(String file)
+        {
+            this.capture = new VideoCapture(file);
+        }
+
+        public Math GetFrame()
+        {
+            return this.capture.QueryFrame();
+        }
+
+        public void Dispose()
+        {
+            this.capture.Stop();
+        }
+    }
+}
