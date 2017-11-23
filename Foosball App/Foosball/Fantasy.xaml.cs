6df9411a89546fd4a33bf48a79f91415f2aabc39
Benas Gudeliauskas <b.gudeliauskas@gmail.com>
2017-11-24 01:18:20 +0200

    Fantasy foosball file

---
+++ Foosball App/Foosball/Fantasy.xaml.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Diagnostics;
+using Foosball_dll.Utils;
+
+using Xamarin.Forms;
+
+//Fantasy Foosball game
+
+namespace Foosball
+{
+    public partial class NewGame : ContentPage
+    {
+
+        public NewGame()
+        {
+            InitializeComponent();
+        }
+
+        // Submit button
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
